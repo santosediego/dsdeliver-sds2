738edb1f788dc7372844a67c4c1ae0a4506b0cf8
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-01-04 20:53:35 -0300

    GET /orders

---
+++ backend/src/main/java/com/santosediego/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.santosediego.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.santosediego.dsdeliver.dto.OrderDTO;
+import com.santosediego.dsdeliver.entities.Order;
+import com.santosediego.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}

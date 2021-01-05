6feb02aea92e0403251c96f099ac9a7762447b9c
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-01-04 21:35:49 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/santosediego/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/santosediego/dsdeliver/controllers/OrderController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +29,18 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<OrderDTO> insert (@RequestBody OrderDTO dto){
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }

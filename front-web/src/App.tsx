70d49d4986dd39d7e1803b034507ee69f55953e4
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-01-06 20:02:14 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Home />
+    </>
   );
 }
 

c34435d2abb9a23dc7f76acf8de264cd17d36d6d
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-01-10 19:24:43 -0300

    Adding orders list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,19 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
 
+    const navigation = useNavigation();
     const handleOnPress = () => {
-
+        navigation.navigate('Orders');
     }
 
     return (
-        <>
+        <> 
+            <Header/>
             <View style={styles.container}>
                 <Image source={require('../assets/deliveryman.png')} />
                 <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
